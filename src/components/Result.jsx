4b7f4c6ccf305cd19c08c8378cdcb58e4cc77c5f
Nikita Kumawat <nikitakumawat44@gmail.com>
2023-10-20 15:49:16 +0530

    feat: add card

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Result({ results }) {
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results.map((curr) => (
-        <div key={curr.id}>{curr.original_title}</div>
+        <Card key={curr.id} result={curr}/>
       ))}
     </div>
   );
